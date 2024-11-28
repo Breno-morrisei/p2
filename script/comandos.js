368cae77223a3d0ba61513078f639e5b796fca1c
Breno-Morrisei <brenofarias438@gmail.com>
2024-11-28 00:33:34 -0300

    Trabalho

--- script/comandos.js
+++ script/comandos.js
@@ -1,3 +1,12 @@
+//HTML Home
+   
+        function irparalogin(){
+            alert('Sucesso');
+            location.href = "login.html"
+        }
+
+// HTML Login
+
         function logar(){
             var login = document.getElementById('Login').value;
             var senha = document.getElementById('Senha').value;
